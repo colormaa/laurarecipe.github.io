72bca6191c79fefa1baaedbd3d4bbe0ba230a04f
boloroo <Boloroo_1993@icloud.com>
2021-05-04 11:01:33 +0800

    commit

---
+++ precache-manifest.377ba174dcdca12e693221bc20506694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e214f6894a3ffd1794fc83e0c60fff13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7f466a7b9ee03590490",
+    "url": "/static/css/main.4fef43f7.chunk.css"
+  },
+  {
+    "revision": "28358170dd17f63cc28a",
+    "url": "/static/js/2.fa99e59e.chunk.js"
+  },
+  {
+    "revision": "f7f466a7b9ee03590490",
+    "url": "/static/js/main.124a14fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
